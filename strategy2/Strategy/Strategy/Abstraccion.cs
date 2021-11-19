efd88f83826fe4e477c7294fa864951b75f94c78
unknown <wmigue@gmail.com>
2021-11-19 11:26:46 -0300

    14

---
+++ strategy2/Strategy/Strategy/Abstraccion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy
+{
+    //hago uso del patron "BRIDGE" para generar una abstraccion de clases suma , resta, etc... solo uso metoto Operar() en el cliente.
+    //desde aqui hago las implementaciones
+    //con encapsulo metodos y clases.(las oculto al cliente)
+    class Abstraccion
+    {
+        int _tipo;
+        double _x, _y;
+        IOperacion _operacion;
+        public Abstraccion(int tipo, double x, double y)
+        {
+            _tipo = tipo;
+            _x = x;
+            _y = y;
+        }
+
+        public double Operar()
+        {
+            if (_tipo == 1)       
+                _operacion = new CSuma();                      
+            if (_tipo == 2)
+                _operacion = new CResta();       
+            if (_tipo == 3)        
+                _operacion = new CMulti();
+            if (_tipo == 4)
+                _operacion = new CDiv();
+
+            return _operacion.Operacion(_x, _y);
+        }
+    }
+}
---
+++ strategy2/Strategy/Strategy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0;
+            double y = 0;
+            string opcion = "";
+
+            
+
+            do
+            {
+                Console.WriteLine("1- Suma, 2- Resta, 3- Multiplicación, 4- División, 5- Salir");
+                opcion = Console.ReadLine();
+                Console.WriteLine("Dame el valor de a");
+                x = double.Parse(Console.ReadLine());
+                Console.WriteLine("Dame el valor de b");
+                y = double.Parse(Console.ReadLine());
+
+                Abstraccion abstraccion = new Abstraccion(Convert.ToInt32(opcion), x, y);
+
+                Console.WriteLine($"El resultado es {abstraccion.Operar()}");
+
+            } while (Int32.Parse(opcion) <= 4);
+        }
+    }
+}
